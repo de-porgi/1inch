7d041fceeadd1bb6d3860242b5f19156a7441853
cyrillicw <cyrillicw@gmail.com>
2021-02-01 23:01:25 +0200

    unnecessary removed

--- contracts/interfaces/IOneInchCaller.sol
+++ contracts/interfaces/IOneInchCaller.sol
@@ -3,19 +3,11 @@
 pragma solidity ^0.6.12;
 pragma experimental ABIEncoderV2;
 
-import "https://github.com/de-porgi/aave_v2/blob/main/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
-import "./IGasDiscountExtension.sol";
-import "./ISafeERC20Extension.sol";
-
-
-interface IOneInchCaller is ISafeERC20Extension, IGasDiscountExtension {
+interface IOneInchCaller {
     struct CallDescription {
         uint256 targetWithMandatory;
         uint256 gasLimit;
         uint256 value;
         bytes data;
     }
-
-    function makeCall(CallDescription memory desc) external;
-    function makeCalls(CallDescription[] memory desc) external payable;
 }
